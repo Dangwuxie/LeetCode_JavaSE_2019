9e24dc62a124a103dbc3166b7c4d4fdba7b08173
dangwuxie <974079012@qq.com>
2019-12-19 23:08:29 +0800

    凑数的一天

---
+++ 程序员代码面试指南/第八章：数组和矩阵问题/src/dxc/PrintArray.java
@@ -0,0 +1,17 @@
+package dxc;
+
+/**
+ * @author 灵魂编程者
+ * @Title: PrintArray
+ * @Description: 天下风云出我辈，一入代码岁月催，
+ * --------------皇图霸业谈笑中，不胜编程一场醉。
+ * @date 2019/9/27 23:53
+ */
+public class PrintArray {
+    public static void printArray(int[] arr){
+        for (int o:arr){
+            System.out.print(o+" ");
+        }
+
+    }
+}
