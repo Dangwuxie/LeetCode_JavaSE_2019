098387e82e96121059b03dc6e79f589e2cf3671f
dangwuxie <974079012@qq.com>
2020-01-15 11:56:27 +0800

    O(N)二分法找出旋转数组的最小数字

---
+++ 剑指offer_2019_1213/src/dxc/no11_旋转数组的最小数字/Test.java
@@ -0,0 +1,16 @@
+package dxc.no11_旋转数组的最小数字;
+
+/**
+ * @author dxc
+ * @Title: Test
+ * @Description: 天下风云出我辈，一入代码岁月催，
+ * --------------皇图霸业谈笑中，不胜编程一场醉。
+ * @date 2020/1/15 11:55
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,4,5,1,2};
+        System.out.println(GetMin.getMin(arr));
+    }
+}
