da85d38deba97223b919539b0827c24c125d66f0
dangwuxie <974079012@qq.com>
2020-01-28 21:42:46 +0800

    修改代码

--- 剑指offer_2019_1213/src/dxc/no20_表示数值的字符串/IsNumeric.java
+++ 剑指offer_2019_1213/src/dxc/no20_表示数值的字符串/IsNumeric.java
@@ -17,6 +17,9 @@ package dxc.no20_表示数值的字符串;
 * 整数部分和指数部分可以是正负开头的0-9的数位串
 * B是0-9的数位串，不能有正负号；
 */
+/*
+* 这题目没做好！
+* */
 public class IsNumeric {
     /*
     * 解题思路：分A\B\C三个部分检查
@@ -32,20 +35,20 @@ public class IsNumeric {
         //首先，扫描整数A部分，可以是-+开头
         //scanA返回值表示如果A部分如果正确，就返回小数点的下标
         //否则就返回-1；
-        int aIsNimeric = scanAC(strs,0);
+        int aIsNumeric = scanAC(strs,0);
 
-        if (aIsNimeric != -1 && aIsNimeric < strs.length && strs[aIsNimeric] == '.'){
+        if (aIsNumeric != -1 && aIsNumeric < strs.length && strs[aIsNumeric] == '.'){
             //如果整数部分正确，而且碰到了小数点就进入此部分
-            aIsNimeric = scanB(strs,aIsNimeric);
+            aIsNumeric = scanB(strs,aIsNumeric);
         }
 
-        if (aIsNimeric != -1 && aIsNimeric < strs.length
-                && (strs[aIsNimeric] == 'e' || strs[aIsNimeric] == 'E')){
+        if (aIsNumeric != -1 && aIsNumeric < strs.length
+                && (strs[aIsNumeric] == 'e' || strs[aIsNumeric] == 'E')){
             //此时说明小数部分也正确，如果存在指数部分，就继续判断
-            aIsNimeric = scanAC(strs,aIsNimeric);
+            aIsNumeric = scanAC(strs,aIsNumeric);
         }
 
-        if (aIsNimeric == -1){
+        if (aIsNumeric == -1){
             return false;
         }else {
             return true;
@@ -57,23 +60,27 @@ public class IsNumeric {
         if (strs[index] == '+' || strs[index] == '-'){
             index++;
         }
-        //此部分可以有正负号
+        //此处不可以可以有正负号，只能有一个符号
         while (index < strs.length){
-            if ((strs[index] >= '0' && strs[index] <= '9') || strs[index] == '.'){
-                if (strs[index] == '.'){
-                    return index;
-                }
+            if (strs[index] >= '0' && strs[index] <= '9'){
                 index++;
+            }else if (strs[index] == '.' || strs[index] != 'e' || strs[index] != 'E'){
+                return index;
+            }else {
+                return -1;
             }
         }
-        return -1;
+        return index;
     }
 
     private static int scanB(char[] strs,int index){
         //次数没有被正负号，碰到e、E就停止
-        while (index < strs.length && strs[index] != 'e' && strs[index] != 'E'){
+        while (index < strs.length){
+
             if (strs[index] >= '0' && strs[index] <= '9'){
                 index++;
+            }else if (strs[index] != 'e' || strs[index] != 'E'){
+                return index;
             }else {
                 return -1;
             }
